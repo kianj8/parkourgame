b54d978a6c68bf48a7af8ced337631eeb3a43dd1
fnaf <42162699+kianj8@users.noreply.github.com>
2023-06-22 14:01:53 -0700

    Add files via upload

---
+++ playerCam.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCam : MonoBehaviour
+{
+    public float sensx;
+    public float sensy;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Lock mouse at center and make invis
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Determines mouseX movment based on input, change in time, and the sensitivity multiplyer. 
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensx;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensy;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate cam
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+}
